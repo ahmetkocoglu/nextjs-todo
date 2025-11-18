261ad7ff0c8ce0f84887d09e52817750a18fd2ac
akocoglu <ahmetkocoglu@outlook.com>
2025-11-18 07:21:44 +0300

    delete and put bugfix

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -3,17 +3,20 @@ import { NextResponse } from 'next/server';
 import { dbConnect } from '@/app/dbConnection';
 import { Todo } from '@/app/todo.model';
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
     await dbConnect();
     const body = await req.json();
-    const updated = await Todo.findByIdAndUpdate(params.id, body, { new: true, runValidators: true });
+    const updated = await Todo.findByIdAndUpdate(id, body, { new: true, runValidators: true });
     if (!updated) return NextResponse.json({ message: 'Not found' }, { status: 404 });
     return NextResponse.json(updated);
 }
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+export async function DELETE(_: Request, { params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
     await dbConnect();
-    const deleted = await Todo.findByIdAndDelete(params.id);
+    console.log(' >> ',id)
+    const deleted = await Todo.findByIdAndDelete(id);
     if (!deleted) return NextResponse.json({ message: 'Not found' }, { status: 404 });
     return NextResponse.json({ ok: true });
 }
